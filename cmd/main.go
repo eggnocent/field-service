df5022936052e84a8f87a117975caa36cb6c7bf8
eggnocent <egiwiratamaaa@gmail.com>
2025-10-26 08:54:03 +0700

    Fix - fixing severals file for deploy

--- cmd/main.go
+++ cmd/main.go
@@ -87,6 +87,10 @@ var command = &cobra.Command{
 			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, PUT")
 			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, x-service-name, x-api-key, x-request-at")
+			if c.Request.Method == "OPTIONS" {
+				c.AbortWithStatus(204)
+				return
+			}
 			c.Next()
 		})
 
